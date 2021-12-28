70caddfeecae2ca7d6b72a7472d8e52081e1bebc
Qworl <83816505+Qworl@users.noreply.github.com>
2021-12-29 02:51:14 +0300

    Update Allocator.h

--- src/Allocator.h
+++ src/Allocator.h
@@ -12,10 +12,8 @@ public:
     size_t used_size = 0;
     size_t peak_size = 0;
     Allocator() = default;
-    Allocator(const size_t total_size) : total_size { total_size }, used_size { total_size }{}
-    ~Allocator() {
-        ::free(buffer_begin);
-    }
+    Allocator(const size_t total_size);
+    ~Allocator();
     struct Header {
     public:
         size_t size;
@@ -26,39 +24,10 @@ public:
     };
     virtual void* allocate(size_t size) = 0;
     virtual void deallocate(void* ptr) = 0;
-    void free() {
-        auto *header = static_cast<Header *>(buffer_begin);
-        header->is_free = true;
-        header->size = (total_size - sizeof(Header));
-        used_size = sizeof(Header);
-    };
+    void free();
     virtual Header *find_block(size_t size) = 0;
-    void split(Header *header, size_t chunk) {
-        //cout << chunk << endl;
-        size_t block_size = header->size;
-        header->size = chunk;
-        header->is_free = false;
-        if (block_size - chunk >= sizeof(Header)) {
-            auto *next = header->next();
-            next->size = block_size - chunk - sizeof(Header);
-            //cout << "()()" << next->size << endl;
-            next->is_free = true;
-            used_size += chunk + sizeof(Header);
-            auto *next_next = next->next();
-        } else {
-            header->size = block_size;
-            used_size += block_size;
-        }
-        //cout << endl;
-    }
-    bool validate(void *ptr) {
-        auto *header = static_cast<Header *>(buffer_begin);
-        while (header < buffer_end) {
-            if (header + 1 == ptr){ return true; }
-            header = header->next();
-        }
-        return false;
-    }
+    void split(Header *header, size_t chunk);
+    bool validate(void *ptr);
     virtual void reset() = 0;
 };
 
